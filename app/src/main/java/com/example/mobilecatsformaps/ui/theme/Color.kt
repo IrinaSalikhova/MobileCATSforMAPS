346d1ccd943861631032edd5fb3b3344ee1f6e70
IrinaSalikhova <irishkaakk@gmail.com>
2024-10-20 00:24:29 -0400

    Colours and font added

--- app/src/main/java/com/example/mobilecatsformaps/ui/theme/Color.kt
+++ app/src/main/java/com/example/mobilecatsformaps/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.example.mobilecatsformaps.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// from https://www.allianceon.org, names fantasised
 val Nyanza = Color(0xFFdbf2d8)   // r="219", g="242", b="216"
 val Celadon = Color(0xFFbbe4b4)   // r="187", g="228", b="180"
 val Cordovan = Color(0xFF783e44)  // r="120", g="62", b="68"
@@ -17,3 +18,17 @@ val HarvestGold = Color(0xFFefab24) // r="239", g="171", b="36"
 val RedCMYK = Color(0xFFe33330)   // r="227", g="51", b="48"
 val KellyGreen = Color(0xFF57b344)  // r="87", g="179", b="68"
 val SpaceCadet = Color(0xFF1f2041)  // r="31", g="32", b="65"
+// from https://carlingtonchc.com, names describe in which elements used
+val LineColor = Color(0xFF05589C)  // hex="#05589c"
+val IconColor = Color(0xFF254D80)  // hex="#254d80"
+val ButtonColor = Color(0xFFCB422F)  // hex="#cb422f"
+val AlertColor = Color(0xFFEEB75C)  // hex="#eeb75c"
+val CategoryColor1 = Color(0xFF555CB6)  // hex="#555cb6"
+val CategoryColor2 = Color(0xFF6D7A04)  // hex="#6d7a04"
+val CategoryColor3 = Color(0xFFB3613B)  // hex="#b3613b"
+val CategoryColor4 = Color(0xFF3F7A80)  // hex="#3f7a80"
+val CategoryColor5 = Color(0xFFAA5074)  // hex="#aa5074"
+val CategoryColor6 = Color(0xFF905396)  // hex="#905396"
+val elementsColor = Color(0xFFB9D9F1)  // hex="#b9d9f1"
+val backgroundColor = Color(0xFFFFFFFF)  // hex="#ffffff"
+val textColor = Color(0xFF2D343A)  // hex="#2d343a"
